ad08eb1cddcd9348a530d706ff59c50e4765a883
IdrisXort <idris.genc@yacht.nl>
2019-10-20 01:34:36 +0200

    added new comp to select

---
+++ src/Components/CycleSelect.vue
@@ -0,0 +1,27 @@
+<template>
+  <select v-model="selectedValue">
+    <option :value="null" disabled>Select a game</option>
+    <option v-for="option in options" :key="option.id" :value="option.id">{{ option.text }}</option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: "CycleSelect",
+  props: {
+    options: {
+      type: Array
+    }
+  },
+  data() {
+    return {
+      selectedValue: null
+    };
+  },
+  watch: {
+    selectedValue(newVal) {
+      if (newVal) this.$emit("selected", newVal);
+    }
+  }
+};
+</script>
